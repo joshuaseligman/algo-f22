e3049ee492740f48f1a70addf71db6ecf3feac43
Josh Seligman <joshua.seligman1@marist.edu>
2022-09-22 07:46:45 -0400

    Cleaned up the output of the main function

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <chrono>
+#include <math.h>
 
 #include "fileUtil.h"
 #include "util.h"
@@ -8,14 +10,29 @@ int main() {
     // Read in the magic items
     StringArr* magicItems = readFile("magicitems.txt");
 
+    // Output some basic stats for comparison to O(n^2) and O(n * log(n)) growth functions
+    std::cout << "n         : " << magicItems->length << std::endl;
+    std::cout << "n^2       : " << magicItems->length * magicItems->length << std::endl;
+    std::cout << "n * log(n): " << magicItems->length * log2(magicItems->length) << std::endl << std::endl;
+
     // Shuffle them and perform selection sort
+    // Time calculations from https://www.geeksforgeeks.org/measure-execution-time-function-cpp/
     knuthShuffle(magicItems);
-    std::cout << selectionSort(magicItems) << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
+    int selectionComparisons = selectionSort(magicItems);
+    auto stop = std::chrono::high_resolution_clock::now();
+    // We need to cast the difference in the stop and start times to microseconds
+    long selectionTime = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+
+    // Print out the results of the selection sort
+    std::cout << "Selection Sort" << std::endl;
+    std::cout << "Comparisons: " << selectionComparisons << std::endl;
+    std::cout << "Time       : " << selectionTime << " μs" << std::endl;
     
     // Print out the list to make sure everything is in order
-    for (int i = 0; i < magicItems->length; i++) {
-        std::cout << magicItems->arr[i] << std::endl;
-    }
+    // for (int i = 0; i < magicItems->length; i++) {
+    //     std::cout << magicItems->arr[i] << std::endl;
+    // }
 
     // Memory management
     delete magicItems;
