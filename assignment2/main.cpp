51dc2275038ccb92bd9d330bb9e4a06b736526b3
Josh Seligman <joshua.seligman1@marist.edu>
2022-09-25 09:31:30 -0400

    Added test data for already sorted and reversed lists for comparisons with shuffled

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -7,10 +7,12 @@
 #include "sortsAndShuffles.h"
 
 // Function to run and print results of a sorting algorithm
-void runSort(int (*sort)(StringArr*), StringArr* input, std::string sortName) {
+void runSort(int (*sort)(StringArr*), StringArr* input, std::string sortName, bool shuffle) {
     // Shuffle them and perform the sort
     // Time calculations from https://www.geeksforgeeks.org/measure-execution-time-function-cpp/
-    knuthShuffle(input);
+    if (shuffle) {
+        knuthShuffle(input);
+    }
     auto start = std::chrono::high_resolution_clock::now();
     int numComparisons = sort(input);
     auto stop = std::chrono::high_resolution_clock::now();
@@ -33,19 +35,36 @@ int main() {
     std::cout << "n^2       : " << magicItems->length * magicItems->length << std::endl;
     std::cout << "n * log(n): " << magicItems->length * log2(magicItems->length) << std::endl << std::endl;
 
+    std::cout << "666 Magic Items, shuffled" << std::endl;
     // Run and analyze selection sort
-    runSort(selectionSort, magicItems, "Selection");
+    runSort(selectionSort, magicItems, "Selection", true);
 
     // Run and analyze insertion sort
-    runSort(insertionSort, magicItems, "Insertion");
+    runSort(insertionSort, magicItems, "Insertion", true);
+
+    std::cout << "20 Yankees Greats, sorted" << std::endl;
+    StringArr* sortedYankees = readFile("yankeesGreatsSorted.txt");
+    runSort(selectionSort, sortedYankees, "Selection", false);
+    runSort(insertionSort, sortedYankees, "Insertion", false);
+
+    std::cout << "20 Yankees Greats, reversed" << std::endl;
+    StringArr* reversedYankees = readFile("yankeesGreatsReversed.txt");
+    runSort(selectionSort, reversedYankees, "Selection", false);
+    delete reversedYankees;
+
+    reversedYankees = readFile("yankeesGreatsReversed.txt");
+    runSort(insertionSort, reversedYankees, "Insertion", false);
+    delete reversedYankees;
+
 
     // Print out the list to make sure everything is in order
-    // for (int i = 0; i < magicItems->length; i++) {
-    //     std::cout << magicItems->arr[i] << std::endl;
+    // for (int i = 0; i < reversedYankees->length; i++) {
+    //     std::cout << reversedYankees->arr[i] << std::endl;
     // }
 
     // Memory management
     delete magicItems;
+    delete sortedYankees;
 
     return 0;
 }
