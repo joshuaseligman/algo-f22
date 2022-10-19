85740ef773605a102bf7629d44ace51ff90bab3c
Josh Seligman <joshua.seligman1@marist.edu>
2022-10-19 11:20:55 -0400

    Generated 42 random magic items

--- assignment3/main.cpp
+++ assignment3/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <set>
+#include <string>
 
 #include "util.h"
 #include "fileUtil.h"
@@ -7,12 +11,23 @@
 int main() {
     StringArr* magicItems = readFile("magicItems.txt");
 
-    quickSort(magicItems);
+    // A set for the random items we want to find. Sets prevent duplicates, so no need to worry about that
+    std::set<std::string> randomItems;
 
-    for (int i = 0; i < magicItems->length; i++) {
-        std::cout << magicItems->arr[i] << std::endl;
+    srand(time(NULL));
+    
+    // Find elements until we have 42 random elements
+    while(randomItems.size() < 42) {
+        int randomIndex = rand() % magicItems->length;
+        randomItems.insert(magicItems->arr[randomIndex]);
     }
 
+    // Print the random elements for now
+    for (auto it = randomItems.begin(); it != randomItems.end(); it++) {
+        std::cout << *it << std::endl;
+    }
+
+    // Memory management
     delete [] magicItems->arr;
     delete magicItems;
 }
