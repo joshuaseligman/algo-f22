12ee9626b0464614b0eeb17bb99efa7761a9cd38
Josh Seligman <joshua.seligman1@marist.edu>
2022-10-22 12:37:12 -0400

    Assigned residents to their top choice

--- finalProject/residentVariation/stableMatching.cpp
+++ finalProject/residentVariation/stableMatching.cpp
@@ -87,115 +87,112 @@ void generateStableMatches(ResidentArr* residents, HospitalArr* hospitals) {
    for (int i = 0; i < residents->length; i++) {
         Node<Hospital*>* cur = residents->arr[i].getHospitalPreferences()->getHead();
 
-        int level = 0;
+        // Add each resident to their top choice hospital
+        cur->data->addResident(&residents->arr[i], 0);
+    }
 
-        while (cur != nullptr) {
-            // Provisionally assign the resident to all of its preferences
-            cur->data->addResident(&residents->arr[i], level);
-
-            level++;
-            cur = cur->next;
-        }
+    for (int i = 0; i < hospitals->length; i++) {
+        hospitals->arr[i].getAssignments()[0].printList();
     }
 
     // A new HospitalArr object whose objects can be moved around without impacting the resident preferences
-    HospitalArr mutableHospitals;
-    mutableHospitals.arr = new Hospital[hospitals->length];
-    mutableHospitals.length = hospitals->length;
+    // HospitalArr mutableHospitals;
+    // mutableHospitals.arr = new Hospital[hospitals->length];
+    // mutableHospitals.length = hospitals->length;
 
-    // Initally set each index to map to the hospital in the main array
-    for (int i = 0; i < hospitals->length; i++) {
-        mutableHospitals.arr[i] = hospitals->arr[i];
-    }
+    // // Initally set each index to map to the hospital in the main array
+    // for (int i = 0; i < hospitals->length; i++) {
+    //     mutableHospitals.arr[i] = hospitals->arr[i];
+    // }
 
-    for (int i = 0; i < mutableHospitals.length; i++) {
-        // Do a sort on the array, ignoring the hospitals already assigned (front of the array)
-        quickSortWithIndices(&mutableHospitals, i, mutableHospitals.length - 1);
+    // for (int i = 0; i < mutableHospitals.length; i++) {
+    //     // Do a sort on the array, ignoring the hospitals already assigned (front of the array)
+    //     quickSortWithIndices(&mutableHospitals, i, mutableHospitals.length - 1);
 
-        // Print out the number of residents in each level for each hospital
-        for (int m = 0; m < mutableHospitals.length; m++) {
-            std::cout << mutableHospitals.arr[m].getName() << "(" << mutableHospitals.arr[m].getCapacity() << ") -  ";
-            for (int n = 0; n < mutableHospitals.arr[m].NUM_LEVELS; n++) {
-                std::cout << mutableHospitals.arr[m].getAssignments()[n].getSize() << " ";
-            }
-            std::cout << "= " << mutableHospitals.arr[m].getNumAssigned() << std::endl;
-        }
+    //     // Print out the number of residents in each level for each hospital
+    //     for (int m = 0; m < mutableHospitals.length; m++) {
+    //         std::cout << mutableHospitals.arr[m].getName() << "(" << mutableHospitals.arr[m].getCapacity() << ") -  ";
+    //         for (int n = 0; n < mutableHospitals.arr[m].NUM_LEVELS; n++) {
+    //             std::cout << mutableHospitals.arr[m].getAssignments()[n].getSize() << " ";
+    //         }
+    //         std::cout << "= " << mutableHospitals.arr[m].getNumAssigned() << std::endl;
+    //     }
 
-        // This is the hospital we are working with
-        Hospital* nextHospital = &mutableHospitals.arr[i];
+    //     // This is the hospital we are working with
+    //     Hospital* nextHospital = &mutableHospitals.arr[i];
 
-        // Create a counter for the number of residents that have been formally assigned to the hospital so far
-        int count = 0;
+    //     // Create a counter for the number of residents that have been formally assigned to the hospital so far
+    //     int count = 0;
 
-        // Iterate through each of the levels from top to bottom (to maximize the preferences)
-        for (int j = 0; j < Hospital::NUM_LEVELS; j++) {
-            nextHospital->getAssignments()[j].printList();
+    //     // Iterate through each of the levels from top to bottom (to maximize the preferences)
+    //     for (int j = 0; j < Hospital::NUM_LEVELS; j++) {
+    //         nextHospital->getAssignments()[j].printList();
 
-            // Start at the head of the list
-            Node<Resident*>* cur = nextHospital->getAssignments()[j].getHead();
+    //         // Start at the head of the list
+    //         Node<Resident*>* cur = nextHospital->getAssignments()[j].getHead();
 
-            // Go through the entire list
-            while (cur != nullptr) {
-                // Check to see if we still have room
-                if (count < nextHospital->getCapacity()) {
-                    // Set the formal assignment
-                    cur->data->setAssignment(nextHospital);
-                    std::cout << "Formally assigned " << cur->data->getName() << " to " << nextHospital->getName() << std::endl;
+    //         // Go through the entire list
+    //         while (cur != nullptr) {
+    //             // Check to see if we still have room
+    //             if (count < nextHospital->getCapacity()) {
+    //                 // Set the formal assignment
+    //                 cur->data->setAssignment(nextHospital);
+    //                 std::cout << "Formally assigned " << cur->data->getName() << " to " << nextHospital->getName() << std::endl;
 
-                    // Increment the counter since one more resident has been formally assigned
-                    count++;
+    //                 // Increment the counter since one more resident has been formally assigned
+    //                 count++;
 
-                    // Remove the resident from all of its other preferences' lists
-                    Node<Hospital*>* removeHospital = cur->data->getHospitalPreferences()->getHead();
-                    int level = 0;
+    //                 // Remove the resident from all of its other preferences' lists
+    //                 Node<Hospital*>* removeHospital = cur->data->getHospitalPreferences()->getHead();
+    //                 int level = 0;
 
-                    while (removeHospital != nullptr) {
-                        // Remove the resident from other hospitals lists
-                        if (removeHospital->data->getName().compare(nextHospital->getName()) != 0) {
-                            removeHospital->data->removeResident(cur->data, level);
-                            std::cout << "Removed " << cur->data->getName() << " from " << removeHospital->data->getName() << std::endl;
-                        }
-                        level++;
-                        removeHospital = removeHospital->next;
-                    }
+    //                 while (removeHospital != nullptr) {
+    //                     // Remove the resident from other hospitals lists
+    //                     if (removeHospital->data->getName().compare(nextHospital->getName()) != 0) {
+    //                         removeHospital->data->removeResident(cur->data, level);
+    //                         std::cout << "Removed " << cur->data->getName() << " from " << removeHospital->data->getName() << std::endl;
+    //                     }
+    //                     level++;
+    //                     removeHospital = removeHospital->next;
+    //                 }
 
-                    cur = cur->next;
-                } else {
-                    // The hospital is full, so have to remove the current resident
-                    Resident* residentToRemove = cur->data;
-                    cur = cur->next;
+    //                 cur = cur->next;
+    //             } else {
+    //                 // The hospital is full, so have to remove the current resident
+    //                 Resident* residentToRemove = cur->data;
+    //                 cur = cur->next;
 
-                    nextHospital->removeResident(residentToRemove, j);
-                    residentToRemove->getHospitalPreferences()->remove(nextHospital);
-                    std::cout << "Removed " << residentToRemove->getName() << " from " << nextHospital->getName() << std::endl;
-                }
-            }
-        }
+    //                 nextHospital->removeResident(residentToRemove, j);
+    //                 residentToRemove->getHospitalPreferences()->remove(nextHospital);
+    //                 std::cout << "Removed " << residentToRemove->getName() << " from " << nextHospital->getName() << std::endl;
+    //             }
+    //         }
+    //     }
 
-        for (int j = 0; j < Hospital::NUM_LEVELS; j++) {
-            nextHospital->getAssignments()[j].printList();
-        }
-        std::cout << std::endl;
-    }
+    //     for (int j = 0; j < Hospital::NUM_LEVELS; j++) {
+    //         nextHospital->getAssignments()[j].printList();
+    //     }
+    //     std::cout << std::endl;
+    // }
 
-    std::cout << "Finished algo" << std::endl << std::endl;
+    // std::cout << "Finished algo" << std::endl << std::endl;
 
-    // Print the final results
-    std::cout << "Final results:" << std::endl;
-    for (int i = 0; i < residents->length; i++) {
-        if (residents->arr[i].getAssignment() != nullptr) {
-            std::cout << "(" << residents->arr[i].getName() << ", " << residents->arr[i].getAssignment()->getName() << ")" << std::endl;
-        } else {
-            std::cout << "(" << residents->arr[i].getName() << ", nullptr)" << std::endl;
-        }
-    }
-    std::cout << std::endl;
+    // // Print the final results
+    // std::cout << "Final results:" << std::endl;
+    // for (int i = 0; i < residents->length; i++) {
+    //     if (residents->arr[i].getAssignment() != nullptr) {
+    //         std::cout << "(" << residents->arr[i].getName() << ", " << residents->arr[i].getAssignment()->getName() << ")" << std::endl;
+    //     } else {
+    //         std::cout << "(" << residents->arr[i].getName() << ", nullptr)" << std::endl;
+    //     }
+    // }
+    // std::cout << std::endl;
 
     // Compute the happiness indices for both residents and hospitals
     std::cout << "Resident Happiness: " << computeResidentHappiness(residents) << std::endl;
     std::cout << "Hospital Happiness: " << computeHospitalHappiness(hospitals) << std::endl;
 
-    delete [] mutableHospitals.arr;
+    // delete [] mutableHospitals.arr;
 }
 
 double computeResidentHappiness(ResidentArr* residents) {
