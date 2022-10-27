a82e671aba8e4623634169918588e83a10cbb2f0
Josh Seligman <joshua.seligman1@marist.edu>
2022-10-27 10:24:30 -0400

    Added comments to the main method

--- assignment4/main.cpp
+++ assignment4/main.cpp
@@ -9,27 +9,35 @@
 int main() {
     StringArr* magicItems = readFile("magicitems.txt");
 
+    // Create the binary search tree
     BinarySearchTree bst;
 
     for (int i = 0; i < magicItems->length; i++) {
-       BinaryTreeNode<std::string>* newNode = new BinaryTreeNode<std::string>(magicItems->arr[i]);
-       bst.insert(newNode);
+        // Add each item to the binary search tree
+        BinaryTreeNode<std::string>* newNode = new BinaryTreeNode<std::string>(magicItems->arr[i]);
+        bst.insert(newNode);
     }
 
     StringArr* searchItems = readFile("magicitems-find-in-bst.txt");
 
+    // Total comparisons starts at 0
     int totalComparisons = 0;
+
     for (int i = 0; i < searchItems->length; i++) {
         int comparisons = 0;
+        // Search the bst for the item
         bool found = bst.search(searchItems->arr[i], &comparisons);
         std::cout << " - " << comparisons << " - " << found << std::endl;
 
+        // Add to the total comparisons
         totalComparisons += comparisons;
     }
 
+    // Compute the average and print it out
     double averageComparisons = (double) totalComparisons / searchItems->length;
     std::cout << "Average comparisons: " << averageComparisons << std::endl;
 
+    // Edge case for an element that doesn't exist in the bst
     std::cout << bst.search("Test") << std::endl;
 
     delete [] searchItems->arr;
