ecdeef14cf1eba1d102659175bafa12e4e6d1f4f
Josh Seligman <joshua.seligman1@marist.edu>
2022-10-27 09:16:39 -0400

    Added the acceptable types for a binary tree

--- assignment4/binaryTree.cpp
+++ assignment4/binaryTree.cpp
@@ -1,6 +1,8 @@
 #include "binaryTree.h"
 #include "binaryTreeNode.h"
 
+#include <string>
+
 template <typename T>
 BinaryTree<T>::BinaryTree() {
     // Initialize the root to be a null pointer
@@ -16,3 +18,7 @@ template <typename T>
 void BinaryTree<T>::setRoot(BinaryTreeNode<T>* newRoot) {
     root = newRoot;
 }
+
+// Define acceptable data types that the binary tree can accept for the template
+template class BinaryTree<std::string>;
+template class BinaryTree<char>;
