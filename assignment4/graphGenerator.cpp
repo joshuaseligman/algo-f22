f3d1580df343931b2fa4f707ac383df82e908618
Josh Seligman <joshua.seligman1@marist.edu>
2022-11-10 16:29:35 -0500

    Wrote a few comments

--- assignment4/graphGenerator.cpp
+++ assignment4/graphGenerator.cpp
@@ -5,12 +5,18 @@
 #include <iostream>
 
 void createGraphs(StringArr* contents) {
+    // Initialize to -1 to denote that no graph has been requested yet
     int curBegin = -1;
+
+    // Go through the file
     for (int i = 0; i < contents->length; i++) {
+        // "new graph" marks the end of the past graph
         if (contents->arr[i].compare("new graph") == 0) {
+            // Only create the graph if there is a previous graph that has been found
             if (curBegin != -1) {
                 std::cout << "New graph" << std::endl;
                 try {
+                    // Mark the graph from "new graph" to the line before the next "new graph"
                     Graph g(contents, curBegin, i - 1);
                     g.printMatrix();
                     g.printAdjacencyList();
@@ -21,10 +27,12 @@ void createGraphs(StringArr* contents) {
                 }
                 std::cout << std::endl;
             }
+            // Set the beginning of the new graph to be the current position
             curBegin = i;
-        } else if (i == contents->length - 1) {
+        } else if (i == contents->length - 1) { // EOF also notes the end of a graph
             std::cout << "New graph" << std::endl;
             try {
+                // The graph is defined in the rest of the file
                 Graph g(contents, curBegin, i);
                 g.printMatrix();
                 g.printAdjacencyList();
